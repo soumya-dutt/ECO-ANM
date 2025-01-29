926b5a9dda9f057ed104b948dbb7d24c72690c60
Jacob Miratsky <98666696+jakemiratsky@users.noreply.github.com>
2025-01-28 22:04:48 -0700

    Create setup.sh
    What dependencies to load before script execution

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ======================================================
+#       Author :      Jacob Miratsky
+#                     Chemistry Ph.D.
+#       Affiliation : ASU, School of Molecular Sciences
+#       Contact :     jacoba1@asu.edu
+#       Usage : Change 'filename'
+# ======================================================
+
+module load vmd-1.9.4-hf
+module load gcc-12.3.0-vw
+module load mamba
+source activate eco_anm
